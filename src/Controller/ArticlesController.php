b22c9676fc04a850fc0a057107cb639bed01f9ac
sungvadan <svd.phan@gmail.com>
2019-08-07 22:21:26 +0200

    add view action

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -11,7 +11,12 @@ class ArticlesController extends AppController
         $this->loadComponent('Paginator');
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
+    }
 
+    public function view($slug  = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
     }
 
 }
